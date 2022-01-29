ade83571e818e9d054f23455988e8d92326bb2d6
Andrew Kan <andrew.m.kan@gmail.com>
2022-01-29 12:59:02 -0500

    Merge commit '051209704056806cee4177fa9f2cbdfc73d449ef' of https://github.com/mowglu/MIAE-Python
    # Conflicts:
    #	api/fetcher.py
    #	api/utils.py

--- api/utils.py
+++ api/utils.py
@@ -1,11 +1,12 @@
 def solid_example_1(*, example_param_1: str, example_param_2: int):
     """
 
-    :param example_param_1: this is the first param
+    :param example_param_1:
     :param example_param_2:
     :return:
     """
-    #TODO: THIS IS A TODO. You must do this.
+    # TODO: Do something... But the technology to do this something comes online in 2030.
+
 
 def solid_example_2(example_param_1: float) -> int:
     """
@@ -14,3 +15,12 @@ def solid_example_2(example_param_1: float) -> int:
     :return:
     """
     return 2
+
+
+def solid_example_3(*, example_param_1: float = 4.0):
+    """
+
+    :param example_param_1:
+    :return:
+    """
+    pass
